ed41ce62b816f65d3dfd9ea6bbbf8de55beed180
Daniel Olavio Ferreira <danielolaviobr@gmail.com>
2022-09-03 12:44:39 +0100

    prisma

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -12,6 +12,6 @@ export const prisma =
     log: ["query"],
   });
 
-if (env.NODE_ENV !== "production") {
-  global.prisma = prisma;
-}
+// if (env.NODE_ENV !== "production") {
+global.prisma = prisma;
+// }
